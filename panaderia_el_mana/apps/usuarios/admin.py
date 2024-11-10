3be1bbddded678b288ad8d18b6c308195f97a2a2
Julian Medina <eljulianmedina07@gmail.com>
2024-11-10 20:41:04 -0300

    Se creo una seccion para iniciar y cerrar sesion

--- panaderia_el_mana/apps/usuarios/admin.py
+++ panaderia_el_mana/apps/usuarios/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from .models import Empleado
+from django.contrib.auth.admin import UserAdmin
+from .models import Empleado#, Usuario
+# from .forms import UsuarioForm
 
 # Register your models here.
 
 admin.site.register(Empleado)
+# admin.site.register(Usuario)
+
+# @admin.register(Usuario)
+# class UsuarioAdmin(UserAdmin):
+#     add_form= UsuarioForm
+#     model = Usuario
+
+#     fieldsets = (
+#         (None, {'fields': ('username', 'password')}),
+#         ('Información Personal', {'fields': ('first_name', 'last_name', 'email','direccion','telefono','fecha_nacimiento','fecha_ingreso')}),
+#         ('Campos Personalizados', {'fields': ('cuit', 'perfil_usuario')}),
+#         ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+#         ('Fechas Importantes', {'fields': ('last_login', 'date_joined')}),
+#     )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('username', 'email', 'cuit','direccion','telefono','fecha_nacimiento','fecha_ingreso', 'perfil_usuario', 'password1', 'password2'),
+#         }),
+#     )
+
+#     list_display = ('username', 'email', 'cuit', 'perfil_usuario', 'is_staff')
+#     search_fields = ('username', 'email', 'cuit')
+#     ordering = ('username',)
